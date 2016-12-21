7b72e9e61024e8dc0a22b59ed53ddaf82d2cf9d7
aaronchan <466638127@qq.com>
2016-12-21 14:16:02 +0800

    static upload1

--- blog/kickstart.js
+++ blog/kickstart.js
@@ -1,22 +1,16 @@
 ﻿var http = require("http");
 var url = require("url");
+var controllers = require("./controllers");
 
-function homeController(req, res) {
-	res.end("home");
-}
-
-function userController(req, res) {
-	res.end("user");
-}
+const rules = [
+{path: "/", controller: controllers.home},
+{path: "/user", controller: controllers.user},
+{path: /^\/static(\/.*)/, controller: controllers.static}
+];
 
 function notFoundController(req, res) {
 	res.end("Not found");
 }
-
-const rules = [
-{path: "/", controller: homeController},
-{path: "/user", controller: userController}
-];
 
 function find(arr, match) {
 	for (var i=0; i<arr.length; i++) {
@@ -27,6 +21,9 @@ function find(arr, match) {
 var server = http.createServer(function (req, res) {
 	var pathname = url.parse(req.url).pathname;
 	var rule = find(rules, function (rule) {
+		if (rule.path instanceof RegExp) {
+			return pathname.match(rule.path);
+		}
 		return rule.path == pathname;
 	});
 	var controller = rule && rule.controller || notFoundController;
